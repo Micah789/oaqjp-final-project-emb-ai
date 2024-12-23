b86d1dd6acee8410400004993280e81e00e39e35
Micah Kwaka <micahkwaka@gmail.com>
2024-12-23 15:11:22 -0500

    extract emotions

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,8 +2,40 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
+    """
+    Emotion detector using the watson emotion api
+
+    Parameters
+    ----------
+    text_to_analyse : string
+                      The text from the input
+
+    Returns
+    -------
+    dict
+        the response from the api
+    """
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     payload = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, data=json.dumps(payload), headers=headers)
-    return response.text
+    
+    # convert the object to dict
+    formatted_response = json.loads(response.text)
+
+    # extract the prediction
+    emotion = list(map(lambda x: x['emotion'], formatted_response['emotionPredictions']))[0]
+
+    return {
+        'anger': emotion['anger'],
+        'disgust': emotion['disgust'],
+        'fear': emotion['fear'],
+        'joy': emotion['joy'],
+        'sadness': emotion['sadness'],
+        'dominant_emotion': max(emotion, key=emotion.get)
+    }
+
+
+
+
+    
