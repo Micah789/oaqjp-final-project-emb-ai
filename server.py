1d8227438940e67ee5174e9f3de9bdc1ce2adcf8
Micah Kwaka <micahkwaka@gmail.com>
2024-12-23 15:54:38 -0500

    obey the pylint rules

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+''' 
+Executing this function initiates the application of sentiment    
+analysis to be executed over the Flask channel and deployed on
+localhost:5000.
+'''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,18 +10,26 @@ app = Flask("Emotion detection")
 
 @app.route('/')
 def render_index_page():
+    """
+    Render and load the home page
+    """
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def sent_emotion():
+    """
+    the emotionDetector which is trigger from clicking the submit button
+    """
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
 
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!."
-    else:
-        return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    return f"""For the given statement,
+    the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 
+    'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. 
+    The dominant emotion is {response['dominant_emotion']}."""
 
 
 if __name__ == "__main__":
